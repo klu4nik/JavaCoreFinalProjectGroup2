741b74d6d3e687396798f781cec1c72022482a5c
Klu4nik <klu4nik.help@gmail.com>
2017-05-01 23:07:02 +0300

    Revert "Delete incorrect directories"
    This reverts commit 1cc0d541f3ba1188f5cd045d15e45428805ef31f.

---
+++ JavaCoreFinalProjectGroup2/src/main/java/com/project/API/API_Hotels.java
@@ -0,0 +1,225 @@
+package com.project.API;
+
+import com.project.Controller.HotelsController;
+import com.project.Entity.Hotel;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Scanner;
+
+/**
+ * Created by MYKOLA.GOROKHOV on 29.04.2017.
+ */
+public class API_Hotels {
+    private final static String ITEM_1 = "1";
+    private final static String ITEM_2 = "2";
+    private final static String ITEM_3 = "3";
+    private final static String ITEM_4 = "4";
+    private final static String ITEM_5 = "5";
+    private final static String EXIT = "q";
+
+    private HotelsController hotelsController = new HotelsController();
+
+    public void run() throws NoSuchAlgorithmException, IOException, ClassNotFoundException {
+        String choice;
+        do {
+            drawMainMenu();
+            //-----------------
+            Scanner scanner = new Scanner(System.in);
+            choice = String.valueOf(scanner.next().toLowerCase().charAt(0));
+            //-----------------
+            switch (choice) {
+                case ITEM_1:
+                    cls();
+                    AddHotelMenu();
+                    break;
+                case ITEM_2:
+                    cls();
+                    editHotelMenu();
+                    break;
+                case ITEM_3:
+                    cls();
+                    DeleteHotelMenu();
+                    break;
+                case ITEM_4:
+                    cls();
+                    findHotelByName();
+                    break;
+                case ITEM_5:
+                    cls();
+                    findHotelByCity();
+                    break;
+                case EXIT:
+                    System.out.println("Exiting . . .");
+                    hotelsController.flush();
+                    break;
+                default:
+                    System.out.println("Wrong Choice");
+                    break;
+            }
+
+        } while (!choice.equals(EXIT));
+    }
+
+    private void editHotelMenu() {
+        System.out.println("+-----------------------------------------+");
+        System.out.println("|             EDIT HOTEL MENU             |");
+        System.out.println("+-----------------------------------------+\n");
+
+        System.out.println("Введите название, редактируемого отеля: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String oldHotelName = scanner.nextLine();
+
+        String oldHotelCity = "";
+        if (!oldHotelName.equals("")) {
+            if (hotelsController.findHotelByName(oldHotelName).size() != 0) {
+                System.out.println("Есть такие варианты:\n" + hotelsController.findHotelByName(oldHotelName));
+                System.out.println("\nВведите город, редактируемого отеля: ");
+                oldHotelCity = scanner.nextLine();
+            } else {
+                System.out.println("\n\nТакого отеля нет в БД . . .");
+                editHotelMenu();
+            }
+        }
+
+        if (!hotelsController.findHotel(new Hotel(oldHotelName, oldHotelCity)).equals(0)) {
+            System.out.println("\nВведите новое название (Enter - оствыить старое) :");
+            String newHotelName = scanner.nextLine();
+            if (newHotelName.equals("")) {
+                newHotelName = oldHotelName;
+            }
+            System.out.println("Новое название отеля: " + newHotelName);
+
+            System.out.println("\nВведите новый город (Enter - оствыить старый) :");
+            String newHotelCity = scanner.nextLine();
+            if (newHotelCity.equals("")) {
+                newHotelCity = oldHotelCity;
+            }
+            System.out.println("Новый город отеля: " + newHotelCity);
+
+            hotelsController.updateHotel(new Hotel(oldHotelName, oldHotelCity),
+                    new Hotel(newHotelName, newHotelCity));
+
+        } else {
+            if (!oldHotelCity.equals("")) {
+                System.out.println("\n\nТакого отеля нет в БД . . .");
+                editHotelMenu();
+            }
+        }
+    }
+
+    private void findHotelByCity() {
+        System.out.println("+-----------------------------------------+");
+        System.out.println("|             FIND HOTEL MENU             |");
+        System.out.println("+-----------------------------------------+");
+        System.out.println("|           FIND HOTEL BY CITY            |");
+        System.out.println("+-----------------------------------------+\n");
+
+        System.out.println("Введите название города: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String hotelCity = scanner.nextLine();
+        if (!hotelCity.equals("")) {
+            System.out.println("Найденые отели: ");
+            System.out.println(hotelsController.findHotelByCity(hotelCity));
+            System.out.println("Для продолжения нажмите Enter");
+            scanner.nextLine();
+        }
+    }
+
+    private void findHotelByName() {
+        System.out.println("+-----------------------------------------+");
+        System.out.println("|             FIND HOTEL MENU             |");
+        System.out.println("+-----------------------------------------+");
+        System.out.println("|           FIND HOTEL BY NAME            |");
+        System.out.println("+-----------------------------------------+\n");
+
+        System.out.println("Введите название отеля: ");
+
+        Scanner scanner = new Scanner(System.in);
+        String hotelName = scanner.nextLine();
+        if (!hotelName.equals("")) {
+            System.out.println("Найденые отели: ");
+            System.out.println(hotelsController.findHotelByName(hotelName));
+            System.out.println("Для продолжения нажмите Enter");
+            scanner.nextLine();
+        }
+    }
+
+    private void DeleteHotelMenu() {
+        System.out.println("+-----------------------------------------+");
+        System.out.println("|           DELETE HOTEL MENU             |");
+        System.out.println("+-----------------------------------------+\n");
+
+        System.out.println("Введите название, удаляемого отеля: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String hotelName = scanner.nextLine();
+        String hotelCity = "";
+        if (!hotelName.equals("")) {
+            System.out.println("Введите город, удаляемого отеля: ");
+            hotelCity = scanner.nextLine();
+        }
+
+        if (!hotelsController.findHotel(new Hotel(hotelName, hotelCity)).equals(0)) {
+            System.out.println("Отель удален");
+            hotelsController.deleteHotel(new Hotel(hotelName, hotelCity));
+        } else {
+            if (!hotelCity.equals("")) {
+                System.out.println("\n\nТакого отеля нет в БД . . .");
+                DeleteHotelMenu();
+            }
+        }
+    }
+
+
+    private void AddHotelMenu() {
+        System.out.println("+-----------------------------------------+");
+        System.out.println("|            ADD HOTEL MENU               |");
+        System.out.println("+-----------------------------------------+\n");
+
+        System.out.println("Введите название, для нового отеля: ");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String hotelName = scanner.nextLine();
+        String hotelCity = "";
+        if (!hotelName.equals("")) {
+            System.out.println("Введите город, для нового отеля: ");
+            hotelCity = scanner.nextLine();
+        }
+
+        if (hotelsController.findHotel(new Hotel(hotelName, hotelCity)).equals(0)) {
+            if (!hotelCity.equals("")) {
+                System.out.println("Отель добавлен");
+                hotelsController.addHotel(new Hotel(hotelName, hotelCity));
+            }
+        } else {
+            System.out.println("\n\nТакой отель уже есть . . .");
+            AddHotelMenu();
+        }
+    }
+
+    private void drawMainMenu() {
+        cls();
+        System.out.println("+-----------------------------------------+");
+        System.out.println("|               HOTEL MENU                |");
+        System.out.println("+-----------------------------------------+");
+        System.out.println("|" + ITEM_1 + ". Добавить отель                        |");
+        System.out.println("|" + ITEM_2 + ". Редактировать данные отеля            |");
+        System.out.println("|" + ITEM_3 + ". Удалить отель                         |");
+        System.out.println("|" + ITEM_4 + ". Поиск отеля по имени                  |");
+        System.out.println("|" + ITEM_5 + ". Поиск отеля по городу                 |");
+        System.out.println("|                                         |");
+        System.out.println("|" + EXIT + ". В предыдущее меню                     |");
+        System.out.println("+-----------------------------------------+");
+    }
+
+    private void cls() {
+        for (int i = 1; i <= 300; i++) {
+            System.out.println();
+        }
+    }
+}
---
+++ JavaCoreFinalProjectGroup2/src/main/java/com/project/DAO/DAO_Users_Impl_TXT.java
@@ -0,0 +1,59 @@
+package com.project.DAO;
+
+import com.project.DAO.DAO;
+import com.project.Entity.User;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * Created by MYKOLA.GOROKHOV on 22.04.2017.
+ */
+public class DAO_Users_Impl_TXT implements DAO<HashMap<String, User>> {
+    final static String PATH = "../DB/Users";
+    final static char SEPARATOR = (char) 29;
+
+    @Override
+    public HashMap<String, User> get() throws IOException {
+//        Читаем файл построчно
+        List<String> currentUsers = null;
+        currentUsers = Files.readAllLines(Paths.get(PATH), StandardCharsets.UTF_8);
+//        Описываем вид результата
+        HashMap<String, User> result = new HashMap<String, User>();
+//        бъем каждую cторку на поля
+        for (String str : currentUsers) {
+            StringTokenizer stringTokenizer = new StringTokenizer(str, SEPARATOR + "");
+            String login = stringTokenizer.nextToken();
+            String firsName = stringTokenizer.nextToken();
+            String lastName = stringTokenizer.nextToken();
+            String password = stringTokenizer.nextToken();
+            result.put(login, new User(firsName, lastName, login, password));
+        }
+        return result;
+    }
+
+
+    @Override
+    public void set(HashMap<String, User> hashMapUsers) {
+        File usersFile = new File(PATH);
+
+        try (FileWriter writer = new FileWriter(usersFile)) {
+            for (HashMap.Entry<String, User> currentEntery : hashMapUsers.entrySet()) {
+                writer.write(
+                        currentEntery.getValue().getLogin() + SEPARATOR +
+                                currentEntery.getValue().getFirsName() + SEPARATOR +
+                                currentEntery.getValue().getLastName() + SEPARATOR +
+                                currentEntery.getValue().getPassword()
+                                + (char) 13 + (char) 10 // Конец строки
+                );
+            }
+        } catch (Exception e) {
+            e.getMessage();
+        }
+    }
+}
